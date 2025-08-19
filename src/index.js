7171bd278afccd58eaad630092f0f8ca4d3b567e
Aleksandra <170029753+AleksandraRog@users.noreply.github.com>
2025-08-19 05:42:19 +0300

    feat: towel-sort

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  return !Array.isArray(matrix)
+    ? []
+    : matrix
+        .map((val, i) =>
+          i % 2 === 0 ? val.sort((a, b) => a - b) : val.sort((a, b) => b - a)
+        )
+        .flat();
 };
